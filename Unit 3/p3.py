a76a3baa204f3d44802f2ee11333e5d718ec0fd7
aravbam774@gmail.com <aravbam774@gmail.com>
2026-03-18 10:27:39 +0530

    Unit 3

--- Unit 3/p3.py
+++ Unit 3/p3.py
@@ -1,6 +1,5 @@
 for i in range(2000, 3200):
     if (i % 7) == 0 and (i & 5)!=0:
          print(i, "is Divisible By 7 But Not multiple by 5 ")
-         break
 else:
     print(i, "is Divisible By & And Multiple Of 5")
